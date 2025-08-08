22f780a9e35ccf97ce563cc4bf451298428cd174
edurikelm <riquelme.mayorga@gmail.com>
2025-08-07 23:29:47 -0400

    se agrega condicion para que aparezca cargando calendario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import CalendarComponent from "@/components/Calendar";
-import { transformarArriendosAEventos } from "@/lib/utils";
-import getArriendos from "@/lib/arriendos";
+import CalendarComponent from '@/components/Calendar';
+import { transformarArriendosAEventos } from '@/lib/utils';
+import getArriendos from '@/lib/arriendos';
 
 export default async function Home() {
-
   const dataFirestore = await getArriendos();
 
   // Transformar los datos a un formato adecuado para el calendario
@@ -11,8 +10,16 @@ export default async function Home() {
 
   return (
     <main className="w-full p-6">
-      <h1 className="text-2xl font-semibold text-neutral-800 dark:text-white mb-6">Mi Calendario de Eventos</h1>
-      <CalendarComponent eventos={eventos} />
+      <h1 className="text-2xl font-semibold text-neutral-800 dark:text-white mb-6">
+        Mi Calendario de Eventos
+      </h1>
+      {eventos.length > 0 ? (
+        <CalendarComponent eventos={eventos} />
+      ) : (
+        <p className="text-neutral-600 dark:text-neutral-400 text-center">
+          Cargando calendario...
+        </p>
+      )}
     </main>
   );
 }
